e57c42b9959e2693c72ae0365be7b722679bbb34
hyerin1228 <hyerin1228@gmail.com>
2024-03-01 19:02:47 +0900

    14. Node.js에서 ECMAScript 모듈 이용하기

---
+++ 01-module-system/https.mjs
@@ -0,0 +1,14 @@
+import { send } from './request.mjs';
+import { read } from './response.mjs';
+// const request = require('./request');
+// const response = require('./response');
+
+function makeRequest(url, data){
+    // 요청을 보내기
+    send(url, data);
+    // 데이터 return 하기
+    return read();
+}
+
+const responseData = makeRequest('https://naver.com', 'any data');
+console.log(responseData);
