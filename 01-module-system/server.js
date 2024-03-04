e7b00d39d1afb6764b7dcb93b10a1358a068bec0
hyerin1228 <hyerin1228@gmail.com>
2024-03-04 16:59:14 +0900

    32. Post 요청으로 데이터 추가하기

--- 01-module-system/server.js
+++ 01-module-system/server.js
@@ -8,14 +8,39 @@
 
 const http = require('http');
 const port = 4000;
+const targetObject = { a: "a", b: "b" };
+
 const server = http.createServer((req, res) => {
     // writeHead는 한 번만 호출되어야 하며 end()가 호출되기 전에 호출되어야 합니다.
     // 상태 코드는와 응답 헤더를 클라이언트에 보냅니다.
-    res.writeHead(200, {
-        // 'Content-Type': 'text/plain'
-        'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify({a: "a", b: "b"}));
+    if(req.method === 'POST' && req.url === '/home'){
+        req.on('data', (data) => {
+            console.log(data);
+            const stringfiedData = data.toString();
+            console.log(stringfiedData);
+            Object.assign(targetObject, JSON.parse(stringfiedData));
+        })
+    }else{
+        if(req.url === '/home'){
+            res.writeHead(200, {
+                // 'Content-Type': 'text/plain'
+                'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify(targetObject));
+        }else if(req.url === '/about'){
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<html>');
+            res.write('<body>');
+            
+            res.write('<h1>About Page</h1>');
+    
+            res.write('</body>');
+            res.write('</html>');
+        }else{
+            res.statusCode = 404;
+            res.end('Not Found');
+        }
+    }
 });
 
 server.listen(port, () => {
