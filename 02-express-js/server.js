5a5468063cdf5b5c9a4fba4f404784be8ef9188e
hyerin1228 <hyerin1228@gmail.com>
2024-03-07 15:32:19 +0900

    42. POST 요청하기 & express.json()

--- 02-express-js/server.js
+++ 02-express-js/server.js
@@ -16,13 +16,46 @@ const Users = [
 
 const app = express();
 
+app.use(express.json()); // body-parser 미들웨어를 사용하여 req.body를 사용할 수 있게 함
+
+app.use((req, res, next) => {
+    const start = Date.now();
+    console.log(`start : ${req.method} ${req.url}`);
+    next();
+    const diffTime = Date.now() - start;
+
+    console.log(`end: ${req.method} ${req.url} ${diffTime}ms`);
+});
+
+
 app.get('/users', (req, res) => {
     res.send(Users);
 });
 
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+
+app.post('/users', (req, res) => {
+    // console.log('req.body.name', req.body.name);
+
+    if(!req.body.name){
+        return res.status(400).json({
+            error: 'Missing user name'
+        })
+    }
+
+    const newUser = {
+        name: req.body.name,
+        id: Users.length
+    };
+
+    Users.push(newUser);
+    res.json(newUser);
+});
+
 
 app.get('/users/:userId', (req, res) => {
     const userId = Number(req.params.userId);
