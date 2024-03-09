ced460cb9bf88a47e9b9443cf0093f7f193775b9
hyerin1228 <hyerin1228@gmail.com>
2024-03-09 22:08:50 +0900

    50. Template Layout 생성하기

--- 02-express-js/controllers/posts.controller.js
+++ 02-express-js/controllers/posts.controller.js
@@ -1,8 +1,11 @@
 const path = require('path');
 
 function getPost(req, res){
+    res.render('posts',{
+        templateName: 'post'
+    })
     // res.send('<div><h1>Post Title</h1><p>This is a post</p></div>');
-    res.sendFile(path.join(__dirname, '..', 'public', 'images', '11891082_1037114966322124_6993873831007562371_n.jpg'));
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', '11891082_1037114966322124_6993873831007562371_n.jpg'));
 }
 
 module.exports = {
--- 02-express-js/server.js
+++ 02-express-js/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = 4000;
 
@@ -6,6 +7,9 @@ const usersRouter = require('./routes/users.router');
 const postsRouter = require('./routes/posts.router');
 
 const app = express();
+
+app.set('view engine', 'hbs'); // 템플릿 엔진을 사용하기 위한 설정
+app.set('views', path.join(__dirname, 'views')); // 템플릿 파일이 위치한 디렉토리를 설정
 
 app.use('/static', express.static('public')); // 정적 파일을 제공하는 미들웨어
 
@@ -18,6 +22,10 @@ app.use((req, res, next) => {
     const diffTime = Date.now() - start;
 
     console.log(`end: ${req.method} ${req.baseUrl} ${diffTime}ms`);
+});
+
+app.get('/', (req, res) => {
+    res.render('index', { imageTitle: "It is a forest" })
 });
 
 app.use('/users', usersRouter);
